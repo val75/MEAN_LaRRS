d2d6f7aac14618ac0ea17ba93b580fda58d3694d
Valentin Ghiur <valentin@testinmotion.com>
2017-07-26 21:56:48 -0700

    Now the controller reads from the DB through the API.

--- public/angular/larrsApp.js
+++ public/angular/larrsApp.js
@@ -3,36 +3,50 @@
  */
 angular.module('larrsApp', []);
 
-var assetListCtrl = function ($scope) {
-    $scope.data = {
-        assets: [{
-            _id: '1234567890',
-            hostname: 'Server1',
-            assetTag: 'ASSET12345',
-            skuModel: 'P1G4',
-            hwModel: 'Model1',
-            mfgName: 'Dell',
-            locName: 'SJC-Lab',
-            groupName: 'DefaultGroup',
-            healthStatus: 'Maintenance',
-            resStatus: 'Free',
-            currentResId: '111222333'
-        }, {
-            _id: '5678901234',
-            hostname: 'Server2',
-            assetTag: 'ASSET67890',
-            skuModel: 'BD1G4',
-            hwModel: 'Model1',
-            mfgName: 'Hyve',
-            locName: 'LVS02',
-            groupName: 'DefaultGroup',
-            healthStatus: 'Maintenance',
-            resStatus: 'Free',
-            currentResId: '44556677'
-        }]
-    };
+var assetListCtrl = function ($scope, larrsData) {
+    $scope.message = "Searching for assets...";
+    larrsData
+        .success(function (data) {
+            $scope.message = data.length > 0 ? "" : "No assets found";
+            var
+                assetList = [];
+            if ( data.length ) {
+                data.forEach(function (doc) {
+                    assetList.push({
+                        _id: doc._id,
+                        hostname: doc.hostname,
+                        assetTag: doc.tag,
+                        skuModel: doc.sku[0].name,
+                        hwModel: doc.hwModel[0].name,
+                        mfgName: doc.manufacturer[0].name,
+                        locName: doc.location[0].name,
+                        groupName: doc.group[0].name,
+                        assetStatus: doc.healthStatus[0].name,
+                        resStatus: doc.reserved,
+                        currentResId: doc.res_id
+                    });
+                });
+            }
+            //console.log(assetList[0].skuModel);
+            $scope.data = { assets: assetList };
+        })
+        .error(function (e) {
+            $scope.message = "Sorry, something's gone wrong";
+            console.log(e);
+        });
+    //$scope.data = { assets: larrsData };
+};
+
+var larrsData = function ($http) {
+    //return $http.get('/api/assets');
+    return $http({
+        method: 'GET',
+        url: '/api/assets',
+        transformResponse: $http.defaults.transformResponse
+    });
 };
 
 angular
     .module('larrsApp')
-    .controller('assetListCtrl', assetListCtrl);
+    .controller('assetListCtrl', assetListCtrl)
+    .service('larrsData', larrsData);
